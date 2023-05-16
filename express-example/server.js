56edf77d64633a4531d0b1c19a590937c40611eb
WeeCrabby <67542282+WeeCrabby@users.noreply.github.com>
2023-05-16 08:57:02 +0200

    backend server makes requests to openaiAPI with custom questions

--- express-example/server.js
+++ express-example/server.js
@@ -11,7 +11,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: "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",
 });
-
+const jsonParser = bodyParser.json()
 
 const openai = new OpenAIApi(configuration);
 const app = express();
@@ -21,24 +21,58 @@ app.get('/', (req, res) => {
 });
 
 //this works, dont touch
-app.post('/', async (req, res) => {
-    const pararms = req.toString();
-    console.log(req.toString());
+app.post('/', jsonParser,async (req, res) => {
+    /*
+    const pararms = req.body;
+    console.log(pararms);
     const gptResponse = await openai.createChatCompletion({
 
         model: 'gpt-3.5-turbo',
 
-        messages: [{role: "user", content: pararms}],
+        messages: [{role: "user", pararms}],
 
 
     });
-    res.send(gptResponse);
 
     // Send a response
-    res.status(200).send('Text received');
-    //res.send(gptResponse.toString());
+    //res.status(200).send('Text received');
 
+    //res.send(gptResponse.toJSON());
+    console.log(gptResponse.data.choices[0].message);
+    res.send(gptResponse.data.choices[0].message);
+*/
+    try {
+
+        const { content } = req.body; // Assuming the incoming JSON has a 'content' field with the text data
+
+        // Generate response from ChatGPT
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo', // Specify the desired ChatGPT model
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are a helpful assistant.', // Optional system message
+                },
+                {
+                    role: 'user',
+                    content: content, // User's text message
+                },
+            ],
+        });
+
+        // Extract the generated answer from the response
+        //const answer = response.choices[0].message.content;
+        const answer = response.data.choices[0].message;
+        // Return the answer as the response
+        res.json({ answer });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'An error occurred while processing the request.' });
+    }
+
+    console.log(req);
 });
+
 
 app.listen(3000, () => console.log('Definition app is listening on port 3000.'));
 app.use(bodyParser.urlencoded({ extended: true }));
